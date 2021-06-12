4ba9f02256cc5c03fa58daa589e74f82e4edd5b4
RizdkyOktaviari <Kudaberkuda@gmail.com>
2021-06-12 11:06:51 +0700

    update middleware 1.0

--- routes/web.php
+++ routes/web.php
@@ -24,23 +24,27 @@ Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
     Route::middleware(['admin'])->group(function () {
-      Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+      //middleware-->Admin
+      Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+      //Route-JenisLayanan
+      Route::get('/JenisLayanan', [App\Http\Controllers\Admin\JenisLayananController::class, 'index'])->name('Home-JenisLayanan');
+      Route::get('/JenisLayanan/create', [App\Http\Controllers\Admin\JenisLayananController::class, 'create'])->name('Create-JenisLayanan');
+      Route::post('/JenisLayanan/store', [App\Http\Controllers\Admin\JenisLayananController::class, 'store'])->name('Store-JenisLayanan');
+
+      Route::get('/JenisLayanan/edit/{id}', [App\Http\Controllers\Admin\JenisLayananController::class, 'edit'])->name('Edit-JenisLayanan');
+      Route::post('/JenisLayanan/update/{id}', [App\Http\Controllers\Admin\JenisLayananController::class, 'update'])->name('Update-JenisLayanan');
+      Route::get('/JenisLayanan/delete/{id}', [App\Http\Controllers\Admin\JenisLayananController::class, 'delete'])->name('Delete-JenisLayanan');
+
+      //prefix-->Admin
       Route::group(['prefix'=>'admin'], function(){
-        Route::get('/users', [App\Http\Controllers\Admin\UsersController::class, 'index'])->name('index-user');
-        Route::get('/users/delete/{id}', [App\Http\Controllers\Admin\UsersController::class, 'delete'])->name('delete-user');
+      Route::get('/users', [App\Http\Controllers\Admin\UsersController::class, 'index'])->name('index-user');
+      Route::get('/users/delete/{id}', [App\Http\Controllers\Admin\UsersController::class, 'delete'])->name('delete-user');
+
+
       });
 
     });
 });
 //Route-User
 Route::get('/dashboard', [App\Http\Controllers\UsersController::class, 'index'])->name('home-user');
-
-//Route-JenisLayanan
-Route::get('/JenisLayanan', [App\Http\Controllers\Admin\JenisLayananController::class, 'index'])->name('Home-JenisLayanan');
-Route::get('/JenisLayanan/create', [App\Http\Controllers\Admin\JenisLayananController::class, 'create'])->name('Create-JenisLayanan');
-Route::post('/JenisLayanan/store', [App\Http\Controllers\Admin\JenisLayananController::class, 'store'])->name('Store-JenisLayanan');
-
-Route::get('/JenisLayanan/edit/{id}', [App\Http\Controllers\Admin\JenisLayananController::class, 'edit'])->name('Edit-JenisLayanan');
-Route::post('/JenisLayanan/update/{id}', [App\Http\Controllers\Admin\JenisLayananController::class, 'update'])->name('Update-JenisLayanan');
-Route::get('/JenisLayanan/delete/{id}', [App\Http\Controllers\Admin\JenisLayananController::class, 'delete'])->name('Delete-JenisLayanan');
