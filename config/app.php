7d35100bf977658d4e476b4e03a624f6f2bce3af
RizdkyOktaviari <Kudaberkuda@gmail.com>
2021-06-25 07:24:14 +0700

    kok eror tadi ges?

--- config/app.php
+++ config/app.php
@@ -233,13 +233,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-<<<<<<< HEAD
-        'Image' => Intervention\Image\Facades\Image::class
-=======
+        'Image' => Intervention\Image\Facades\Image::class,
+
         'PDF' => Barryvdh\DomPDF\Facade::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+      ],
 
->>>>>>> ab26f15439054e69d80375782e9a4f1a570cf9f9
-    ],
-
-];
+  ];
